0094db1162e830f33772c6ad7526be9907528e4f
Dhanushwar <110483463+Dhanushwar-M@users.noreply.github.com>
2022-12-20 04:19:50 -0800

    Update Transpose_Matrix.java

--- Day_5/Transpose_Matrix.java
+++ Day_5/Transpose_Matrix.java
@@ -1,12 +1,10 @@
-  public int[][] transpose(int[][] A) {
-        if(A == null || A.length == 0 || A[0].length == 0)
-            return new int[0][0];
-        int m = A.length, n = A[0].length;
-        int[][] result = new int[n][m];
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                result[j][i] = A[i][j];
+   public int[][] transpose(int[][] A) {
+        int m = A.length, n = m == 0 ? 0 : A[0].length;
+        int[][] res = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                res[i][j] = A[j][i];
             }
         }
-        return result;
+        return res;
     }
